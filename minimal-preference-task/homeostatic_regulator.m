f3f57ac4366a2902698c60b4b5005bf548db7b23
Miguel Aguilera <miguel@maguilera.net>
2015-04-24 08:03:03 +0200

    Update homeostatic_regulator.m

--- minimal-preference-task/homeostatic_regulator.m
+++ minimal-preference-task/homeostatic_regulator.m
@@ -2,8 +2,6 @@ function y=homeostatic_regulator(x,H1,H2)
 
     x=mod(x+pi,2*pi)-pi;
     
-    [n,m]=size(x);
-    
     y=(abs(x)-H1*pi)/H2/pi;
     y(abs(x)<=(H1*pi))=0;
     y(abs(x)>(H1*pi+H2*pi))=1;
